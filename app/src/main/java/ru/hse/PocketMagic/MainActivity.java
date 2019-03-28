236ee5221e33ffe6c10b8340bff2068dd6304891
Lev Levin <combolev@gmail.com>
2019-03-28 15:56:27 +0300

    Initial commit

---
+++ app/src/main/java/ru/hse/PocketMagic/MainActivity.java
@@ -0,0 +1,38 @@
+package ru.hse.PocketMagic;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
+
+        Button flameButton = findViewById(R.id.flameButton);
+        flameButton.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animAlpha);
+            }
+        });
+
+        Button spellsButton = findViewById(R.id.spellsButton);
+        spellsButton.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+}
