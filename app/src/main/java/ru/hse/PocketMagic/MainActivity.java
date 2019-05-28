f0772349b563220f0d36eebc42e092f9d93485af
Lev Levin <combolev@gmail.com>
2019-05-28 16:06:50 +0300

    Add bot Button

--- app/src/main/java/ru/hse/PocketMagic/MainActivity.java
+++ app/src/main/java/ru/hse/PocketMagic/MainActivity.java
@@ -20,12 +20,24 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        final Animation animSize = AnimationUtils.loadAnimation(this, R.anim.size);
 
         Button flameButton = findViewById(R.id.flameButton);
         flameButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
                 view.startAnimation(animAlpha);
+                /*Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+                finish();*/
+            }
+        });
+
+        Button botButton = findViewById(R.id.botButton);
+        botButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animSize);
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(intent);
                 finish();
